297648b30c9b0131dbf9331c9cb920431a70bdc8
felipe-alheiro <felipe_alheiro@hotmail.com>
2021-03-23 13:09:07 -0300

    Add files via upload

---
+++ CRUDLuisExercicio03/src/main/java/app/Principal.java
@@ -0,0 +1,19 @@
+package app;
+
+import service.Servico;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Servico s = new Servico();
+		s.abrirTransacao();
+		s.fecharSessao();
+		
+		
+		
+		
+		
+	}
+
+}
